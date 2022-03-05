33c6cb12d2631925ee91935890614c048aaf9f46
oriostejason <71482613+oriostejason@users.noreply.github.com>
2022-03-05 23:28:29 +0800

    calendar

--- Admin/layout.part/admin.header.php
+++ Admin/layout.part/admin.header.php
@@ -9,11 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"/>
-    <link rel="stylesheet" href="style/dataTables.bootstrap5.min.css" />
-    <link rel="stylesheet" href="style/admin.style.css" />
-
+  
     <!-- For Calendar -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
@@ -22,7 +18,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
 
-    <!-- Template Stylesheet -->
-    <link href="tyle/admin.style.css" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"/>
+    <link rel="stylesheet" href="style/dataTables.bootstrap5.min.css" />
+    <link rel="stylesheet" href="style/admin.style.css" />
+
+   
 
   </head>
