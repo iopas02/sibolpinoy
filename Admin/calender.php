f3f6af7ba9cfccfde316530f48733aa336fc44be
oriostejason <71482613+oriostejason@users.noreply.github.com>
2022-03-05 23:10:37 +0800

    update

--- Admin/calender.php
+++ Admin/calender.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <html lang="en">
   
-  
   <!-- Header Start -->
   <?php
       require "layout.part/admin.header.php";
@@ -10,16 +9,96 @@
 
   <body>
     <title>Sibol-PINOY Calendar</title>
+
     <script>
-
-      document.addEventListener('DOMContentLoaded', function() {
-        var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth'
+   
+      $(document).ready(function() {
+        var calendar = $('#calendar').fullCalendar({
+        editable:true,
+        header:{
+          left:'prev,next today',
+          center:'title',
+          right:'month,agendaWeek,agendaDay'
+        },
+        events: 'load.php',
+        selectable:true,
+        selectHelper:true,
+        select: function(start, end, allDay)
+        {
+          var title = prompt("Enter Event Title");
+          if(title)
+          {
+          var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+          var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+          $.ajax({
+            url:"insert.php",
+            type:"POST",
+            data:{title:title, start:start, end:end},
+            success:function()
+            {
+            calendar.fullCalendar('refetchEvents');
+            alert("Added Successfully");
+            }
+          })
+          }
+        },
+        editable:true,
+        eventResize:function(event)
+        {
+          var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+          var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+          var title = event.title;
+          var id = event.id;
+          $.ajax({
+          url:"update.php",
+          type:"POST",
+          data:{title:title, start:start, end:end, id:id},
+          success:function(){
+            calendar.fullCalendar('refetchEvents');
+            alert('Event Update');
+          }
+          })
+        },
+    
+        eventDrop:function(event)
+        {
+          var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+          var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+          var title = event.title;
+          var id = event.id;
+          $.ajax({
+          url:"update.php",
+          type:"POST",
+          data:{title:title, start:start, end:end, id:id},
+          success:function()
+          {
+            calendar.fullCalendar('refetchEvents');
+            alert("Event Updated");
+          }
+          });
+        },
+    
+        eventClick:function(event)
+        {
+          if(confirm("Are you sure you want to remove it?"))
+          {
+          var id = event.id;
+          $.ajax({
+            url:"delete.php",
+            type:"POST",
+            data:{id:id},
+            success:function()
+            {
+            calendar.fullCalendar('refetchEvents');
+            alert("Event Removed");
+            }
+          })
+          }
+        },
+    
         });
-        calendar.render();
       });
-
+      
     </script>
 
     <!-- top navigation bar -->
@@ -54,6 +133,8 @@
     <?php
       require "layout.part/admin.footer.php";
     ?>
+
+    </script>
 
     <!-- Footer and JS Script End Here -->
 
--- Admin/layout.part/admin.header.php
+++ Admin/layout.part/admin.header.php
@@ -14,9 +14,14 @@
     <link rel="stylesheet" href="style/dataTables.bootstrap5.min.css" />
     <link rel="stylesheet" href="style/admin.style.css" />
 
-    <!-- for calendar Stylesheet -->
-    <link href='fullcalendar/main.css' rel='stylesheet' />
-    
+    <!-- For Calendar -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+
     <!-- Template Stylesheet -->
     <link href="tyle/admin.style.css" rel="stylesheet">
 
