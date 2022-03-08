ee40e7b4b72f679732c88f1d91dc6005c5bfbb61
Maykelzzz <mz.game0@gmail.com>
2022-03-08 14:17:00 +0800

    dropdown

--- Admin/layout.part/admin.top.navbar.php
+++ Admin/layout.part/admin.top.navbar.php
@@ -20,7 +20,7 @@
             </div>
           </form>
 
-          <!-- script -->
+          
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
       
             <li class="nav-item dropdown">
