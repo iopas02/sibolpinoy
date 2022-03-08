9f32a8db2fae4839ea36ed1f63c7b478e7f580fc
Maykelzzz <mz.game0@gmail.com>
2022-03-08 14:04:41 +0800

    topnavbar

--- Admin/layout.part/admin.top.navbar.php
+++ Admin/layout.part/admin.top.navbar.php
@@ -20,15 +20,17 @@
             </div>
           </form>
 
-          <ul class="navbar-nav">
+          <!-- script -->
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+      
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle ms-2 bg-blue" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a class="nav-link dropdown-toggle ms-1 bg-blue" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 <i class="bi bi-person-fill"></i>
               </a>
               <ul class="dropdown-menu dropdown-menu-end">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                <li><a class="dropdown-item" href="#">Change Password</a></li>
+                
               </ul>
             </li>
           </ul>
