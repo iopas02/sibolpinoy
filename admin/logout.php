23abb1dc971ff7e5be19e470eee9b3aa0ce0d0e9
Charles Daniel Abuzo <abuzo.daniel@gmail.com>
2022-03-08 19:01:28 +0800

    ok na

--- admin/logout.php
+++ admin/logout.php
@@ -1,6 +1,13 @@
 <?php
-session_start();
+require "layout.part/admin.header.php";
 session_destroy();
-header("location: index.php");
+header( "refresh:3;url=index.php" );
+echo "  <div class='loader_bg'>
+            <div class='welcome'>
+                <h2>Successfully Logged out! Redirecting to login...</h2>
+            </div>
+            <div class='loader mt-5'></div>
+        </div>
+    ";
 exit();
 ?>
