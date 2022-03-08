127e5fd99ca48ea618d4cd6043e96f4664c53eef
Charles Daniel Abuzo <abuzo.daniel@gmail.com>
2022-03-08 22:28:23 +0800

    mema lang

--- admin/admin.con.php
+++ admin/admin.con.php
@@ -31,7 +31,12 @@ if(isset($_GET["error"])){
 ?>
 <!doctype html>
 <html lang="en">
-  
+    <div class='loader_bg'>
+        <div class='welcome'>
+            <h2>Loading...</h2>
+        </div>
+        <div class='loader mt-5'></div>
+    </div>
   <!-- Header Start -->
   <?php
       require "layout.part/admin.header.php";
