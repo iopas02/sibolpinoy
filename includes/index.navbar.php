45e8285b74a4e802e8248981a4c56bcb5dc6e306
Kenneth <kennethpunzalan20@gmail.com>
2022-03-09 17:52:42 +0800

    pushpush

---
+++ includes/index.navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+    <a href="index.php" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+        <img class="__logo" src="img/logo.png" alt=""><h2 class="m-0 text-dark"></i>Sibol-Pinoy</h2>
+    </a>
+    <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto p-4 p-lg-0">
+            <a href="index.php" class="nav-item nav-link active">Home</a>
+            <a href="#about" class="nav-item nav-link">About</a>
+            <a href="#services" class="nav-item nav-link">Services</a>
+            <a href="event.php" class="nav-item nav-link">Events</a>
+            <a href="contact.php" class="nav-item nav-link">Contact</a>
+        </div>
+        <!-- <a href="#" class="btn py-4 px-lg-5 d-none d-lg-block book-consult">Book Consultation<i class="fa fa-arrow-right ms-3"></i></a> -->
+        <a href="#" class="btn py-4 px-lg-5 d-none d-lg-block bg-yellow">Book Consultation<i class="fa fa-arrow-right ms-3 text-light"></i></a>
+    </div>
+</nav>
