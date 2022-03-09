98a2f8f19358de563fdc55985522f77968f5b1ec
oriostejason <71482613+oriostejason@users.noreply.github.com>
2022-03-09 14:32:48 +0800

    push ko lang

--- admin/layout.part/admin.header.php
+++ admin/layout.part/admin.header.php
@@ -37,7 +37,9 @@ session_start();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"/>
     <link rel="stylesheet" href="style/dataTables.bootstrap5.min.css" />
+
     <link rel="stylesheet" href="style/admin.style.css" />
+    <link rel="stylesheet" href="style/colored.css" />
     
   </head>
   
