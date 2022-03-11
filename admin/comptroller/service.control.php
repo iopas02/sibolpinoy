b3893cd231d7f5ae7d52d147ac668ffe62c8f676
oriostejason <71482613+oriostejason@users.noreply.github.com>
2022-03-11 20:03:19 +0800

    push mo yan

---
+++ admin/comptroller/service.control.php
@@ -0,0 +1,6 @@
+<?php
+ include_once('../connection.php');
+
+ if(isset($_POST['create_services'])){
+     echo "Your connected!";
+ }
--- admin/services.tools.php
+++ admin/services.tools.php
@@ -26,7 +26,7 @@
 
     <main class="mt-5 pt-3">
         <nav class="navbar navbar-expand-lg navbar-light bg-white">
-            <div class="container-fluid">
+            <div class="container-fluid border-bottom border-3 border-dark mb-2">
                 <a class="navbar-brand" href="services.tools.php">Services Tools</a>
                 
                 <div class="navbar-nav">
@@ -34,6 +34,36 @@
                 </div>
             </div>
         </nav>
+        <div class="row col-md-12">
+            <div class="row col-md-12 px-5">
+                    <h5>Create Services</h5>
+            </div>
+            <div class="row col-md-12">
+                <form action="comptroller/service.control.php" method="POST">
+                    <div class="row col-md-12">
+                        <div class="col-md-4 m-2">
+                            <label for="formFile" class="form-label">Insert Image</label>
+                            <input class="form-control" type="file" id="formFile" name="formFile">
+                        </div>
+                        <div class="col-md-6 m-2">
+                            <label for="service_title" class="form-label">Sevice Title</label>
+                            <input type="text" class="form-control" id="service_title" name="service_title" placeholder="Sevice Title">
+                        </div>
+                    </div>
+                    <div class="row col-md-12">
+                        <div class="col-md-11 m-2">
+                            <label for="service_desc" class="col-form-label">Description:</label>
+                            <textarea class="form-control" id="service_desc" name="service_desc" rows="5" placeholder="Type services Description"></textarea>
+                        </div>
+                    </div>
+                    <div class="row col-md-12">
+                        <button type="submit" name="create_services" class="btn bg-coloured text-white col-md-3 my-2 mx-4" >
+                            Create Services
+                        </button>
+                    </div>
+                </form>
+            </div> 
+        </div>
     
     </main>
 
