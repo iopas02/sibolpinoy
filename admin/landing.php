787489b50ef5cf90fc0ec02bea2faada8b01b6e4
Maykelzzz <mz.game0@gmail.com>
2022-03-14 23:44:01 +0800

    history / events

--- admin/landing.php
+++ admin/landing.php
@@ -214,37 +214,27 @@
           </div>
         </div>
 
-        <div class="col-md-3 mb-3">
+        <div class="col-md-3 mt-4">
           <div class="container">
-            <div class="row ">
-               <?php
-                  date_default_timezone_set("Asia/Manila");
-                  $year = date("Y");
-                  $month = date("M");
-                  $date = date("d");
-                  $time = date("g:i a");  
-                ?>
-              <div class="col-lg-12 text-center bg-coloured text-white page-header" style="border-top-right-radius: 45px;">
-                <?php echo $year ?>
-              </div>
-              <div class="col-md-4 bg-coloured text-white" style="border-bottom-left-radius: 45px;">
+            <div class="row">
+              <div class="col-lg-12 text-center py-2 bg-coloured text-white card-text">
+                Recent Action
+                </div>
+              <div class="col-md-6 bg-coloured text-white">
               <hr class="dropdown-divider bg-light" />
-                  <div class="text-center py-5">
-                    <div class="count-text"><?php echo $date ?></div>
-                    <div class="user-text"> <?php echo $month ?></div>
-                  </div>
-              </div>
-              <div class="col-md-8">
-                <h3 class="page-header">Event for Today</h3>
-
-                <div class="card-text">
-                  ISO 9001:2015 Requirements and Internal Quality Audit
+                  <div class="text-center py-3">
                 </div>
+              <!-- Username -->
+              <h6 class="px-3 py-1">Username:
+              <?= $rusername ?>
+              </h6> 
+              <!-- Action -->
+              <h6 class="px-3 py-1">Action:
 
-                <div class="pt-3 login-text">
-                  <img src="svg/watch.svg" style="width: 18px; height: 18px;" alt="" /> <?php echo $time ?>
-                </div>
-              </div>
+              </h6>
+              <!-- Date and Time -->
+              <h6 class="px-3 py-1">Date & time:               
+              </h6>
             </div>
           </div>
         </div>
@@ -310,21 +300,46 @@
           </div>
         </div>
 
-        <div class="col-md-6 mb-3">
+        <div class="col-md-3 mb-3">
           <div class="card h-100">
-            <div class="container pt-3 pb-3 bg-light" id="about">
+            <!-- <div class="container pt-6 pb-3 bg-light"> -->
                 <div class="container">
-                    <div class="row g-5">
-                        <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
-                            <h6 class="text-dark pe-3 secondary-font">We Celebrate on this Month</h6>
-                            <h1 class="header-font">Happy Mother's Day</h1>
-                            <h5 class="second-header">A simple Message and Celebration From Sibol-Pinoy</h5>
-                        </div>
-                        <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s"  >
-                            <div class="position-relative h-50">
-                                <img class="prev-gif"  src="../img/celebrate_1.gif" alt="">
+                    <div class="row g-7">
+                        <div class="col-lg-9 wow fadeInUp" data-wow-delay="0.3s">
+                            <h4 class="text-dark pe-6 primary-font">Coming Events</h4>
+                            <h6 class="header-font">Mother's Day</h6>
+                            <p class="second-header">A simple Message and Celebration From Sibol-Pinoy</p>
+                               <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#Events">
+                               View Details 
+                            </button>
+
+                            <div class="modal fade" id="Events" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                              <div class="modal-dialog">
+                                <div class="modal-content">
+                                  <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Happy Mother's Day</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                    <p>Greetings for the best mom's in the world from SPMC Philippines.
+                                      <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s"  >
+                                      <div class="position-relative h-50">
+                                      <img class="prev-gif"  src="../img/celebrate_1.gif" alt="Mother's Day">
+                                      </div>
+                                      </div></p>
+                                    </div>
+                                    
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                    <!-- Edit -->
+                                    <button type="button" class="btn btn-primary">Edit</button> 
+                                    <!-- Add -->
+                                    <button type="button" class="btn btn-success">Add</button> 
+                                  </div>
+                                </div>
+                              </div>
                             </div>
-                        </div>
+                          </div>
                     </div>
                 </div>
             </div>
